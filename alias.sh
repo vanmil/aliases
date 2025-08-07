1bba7796cb666bcb61d6a8941f95c6d8b5a9227c
vanmil <jellevanmil@gmail.com>
2025-08-07 11:40:18 +0200

    Add zip

--- alias.sh
+++ alias.sh
@@ -1,1 +1,7 @@
+## Diskoverview
 diskoverview="du -h --max-depth=1 --exclude=./diskstation | sort -hr"
+
+## ZIP
+# The syntax is ' zip -r <zip file name> <directory name> '. The '-r' option tells zip to include files/folders in sub-directories. Use quotes around the zip file or folder name if the name contains any spaces.
+zipfolder="zip -r"
+zipexample="echo zip -r <zip file name> <directory name>"
