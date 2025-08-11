113a5a3035c0d1376826e0c737f71f59d7e715d9
vanmil <jellevanmil@gmail.com>
2025-08-11 09:54:27 +0200

    Update alias.sh whichubuntu

--- alias.sh
+++ alias.sh
@@ -5,3 +5,6 @@ diskoverview="du -h --max-depth=1 --exclude=./diskstation | sort -hr"
 # The syntax is ' zip -r <zip file name> <directory name> '. The '-r' option tells zip to include files/folders in sub-directories. Use quotes around the zip file or folder name if the name contains any spaces.
 zipfolder="zip -r"
 zipexample="echo zip -r <zip file name> <directory name>"
+
+# Version related
+whichubuntu="lsb_release -a"
